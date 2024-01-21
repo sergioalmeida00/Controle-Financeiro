0b5ecb0bf300ca538858c8198b871e0c30684f6a
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2024-01-20 23:01:00 -0300

    FEAT: adicionado o endpoint de logout

--- app/Http/Controllers/Api/User/AuthController.php
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -23,48 +23,10 @@ class AuthController extends Controller
         ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function logout()
     {
-        //
-    }
+        auth()->logout();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return response()->json(['message' => 'Successfully logged out']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', [AuthController::class, 'login'])->name('auth.login');
 
+
 // Rotas protegidas por JWT
 Route::middleware('auth:api')->group(function () {
-
+    Route::post('auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
     // Usuario
     Route::prefix('user')->group(function () {
         Route::post('/create', [UserController::class, 'store']);
